5d3ebdabe3a7739cd5b932a3b896d0a540b39ce2
w6688j <953372680@qq.com>
2019-03-26 01:36:21 +0800

    create image

--- reptile.py
+++ reptile.py
@@ -5,26 +5,42 @@ from bs4 import BeautifulSoup
 # 引入selenium
 from selenium import webdriver
 
-response = requests.get('http://hanlp.com/')
-soup = BeautifulSoup(response.text, 'html.parser', from_encoding='utf-8')
-iframes = soup.find_all('iframe')
 
-# 使用webkit无界面浏览器
-# 如果路径为 exe 启动程序的路径，那么该路径需要加一个 r
-driver = webdriver.PhantomJS(executable_path=r'F:/CodeSoftware/phantomjs/bin/phantomjs.exe')
-# 获取指定网页的数据  start_urls
-driver.get(iframes[1]['src'])
-driver.maximize_window()
+def getQuestions(dir):
+    questions = []
+    with open(dir, 'r', encoding='utf-8') as f:
+        for line in f:
+            questions.append(line.strip())
 
-element = driver.find_element_by_class_name('hasSVG')
-left = element.location['x']
-top = element.location['y']
-right = element.location['x'] + 540
-bottom = element.location['y'] + 130
+    return questions
 
-driver.save_screenshot('E:/Projects/PyCharmProjects/GitHub/TripleIE/images/1.png')
-driver.close()
 
-im = Image.open('E:/Projects/PyCharmProjects/GitHub/TripleIE/images/1.png')
-im = im.crop((left, top, right, bottom))
-im.save('E:/Projects/PyCharmProjects/GitHub/TripleIE/images/1.png')
+questions = getQuestions('data/question.txt')
+
+for (i, question) in enumerate(questions):
+    print('====== Start Create Image_%s ======' % i)
+
+    response = requests.get('http://hanlp.com/?sentence=' + question)
+    soup = BeautifulSoup(response.text, 'html.parser', from_encoding='utf-8')
+    iframes = soup.find_all('iframe')
+
+    # 使用webkit无界面浏览器
+    # 如果路径为 exe 启动程序的路径，那么该路径需要加一个 r
+    driver = webdriver.PhantomJS()
+    driver.set_window_size(660, 130)
+    # 获取指定网页的数据  start_urls
+    driver.get(iframes[1]['src'])
+    driver.maximize_window()
+
+    element = driver.find_element_by_class_name('hasSVG')
+    left = element.location['x']
+    top = element.location['y']
+    right = element.location['x'] + 660
+    bottom = element.location['y'] + 130
+
+    driver.save_screenshot('images/%s.png' % i)
+    driver.close()
+
+    im = Image.open('images/%s.png' % i)
+    im = im.crop((left, top, right, bottom))
+    im.save('images/%s.png' % i)
